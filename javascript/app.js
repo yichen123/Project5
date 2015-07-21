266add1675b3c137318260fce7577fc3bc7ad203
yichen123 <chenyi330@hotmail.com>
2015-07-21 15:48:15 +0800

    some changes

---
+++ javascript/app.js
@@ -0,0 +1,11 @@
+function init() {
+  var mapOptions = {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
+    zoom: 8
+  };
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', init);
