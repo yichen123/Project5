63c54fd0594c8e77c7a479c9fc1b57cffbd05b8d
yichen123 <chenyi330@hotmail.com>
2015-07-23 09:13:56 +0800

    changed the map latlng to china

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
-function init() {
-  var mapOptions = {
+// data
+var initialStatus = {
     center: {
-      lat: -34.397,
-      lng: 150.644
+        lat: 40.1431473,
+        lng: 116.2907178
     },
-    zoom: 8
-  };
-  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    zoom: 13
+};
+
+//classes
+
+function init() {
+    var map = new google.maps.Map(document.getElementById('map'), initialStatus);
+    var marker = new google.maps.Marker({
+        position: {
+            lat: 40.1431473,
+            lng: 116.2907178
+        },
+        map: map,
+    });
 }
 google.maps.event.addDomListener(window, 'load', init);
