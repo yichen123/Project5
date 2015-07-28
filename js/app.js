db23649bd038a6fcc475759f0992b359d20ea003
yichen123 <chenyi330@hotmail.com>
2015-07-28 23:51:52 +0800

    need more work on process flow

--- js/app.js
+++ js/app.js
@@ -1,14 +1,20 @@
 // data
 var initialStatus = {
     center: {
-        lat: 39.94,
-        lng: 116.4
+        lat: 40.715586,
+        lng: -73.995174
     },
-    zoom: 13
+    zoom: 14,
+    keyword: 'hotpot',
+    limit: '10'
 };
 
-var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=CE2VKST0IIJ11AELGRTJBBWIFXJXVMWTPE0RW1AXPTWJN22M&client_secret=UAJNPU23B3TPFUBPFLOYECCQPSAS3CPPOMLQXK5EHRBITHAO&v=20130815&ll=39.94,116.4&query=pekingduck'
+var fourSquareURL = 'https://api.foursquare.com/v2/venues/search?client_id=CE2VKST0IIJ11AELGRTJBBWIFXJXVMWTPE0RW1AXPTWJN22M&client_secret=UAJNPU23B3TPFUBPFLOYECCQPSAS3CPPOMLQXK5EHRBITHAO&v=20130815&ll=' + initialStatus.center.lat + ',' + initialStatus.center.lng + '&query=' + initialStatus.keyword + '&limit=' + initialStatus.limit;
 
+// array for store data from foursquare
+var apiResult = [];
+
+// array for store markers
 var markers = [];
 //classes
 
@@ -18,43 +24,56 @@ var markers = [];
 function modelView() {
     var self = this;
     var map = init();
+    var marker = new google.maps.Marker({
+        name: 'test1',
+        position: {
+            lat: 40.71,
+            lng: -73.995
+        },
+    });
+    marker.setMap(map);
+    fsInit(map);
     self.map = ko.observable(map);
     self.search = ko.observable();
 
 }
 
-function fsInit() {
+function fsInit(map) {
     var result = $.getJSON(fourSquareURL, function(data) {
         var spots = data.response.venues;
-        for (var i = 0, len = Math.min(9, spots.length); i < len; i++) {
-            var location = spots[i].location;
+        for (var i = 0, len = spots.length; i < len; i++) {
+            var place = spots[i];
             var marker = {
-                'name': location.address,
-                'lat': location.lat,
-                'lng': location.lng
+                'name': place.name,
+                'lat': place.location.lat,
+                'lng': place.location.lng
             }
-            markers.push(marker);
+            apiResult.push(marker);
             console.log(spots[i]);
             console.log(marker);
-
         }
+        console.log(apiResult);
+        for (var i = 0, len = apiResult.length; i < len; i++) {
+            var that = apiResult[i];
+            marker[i] = new google.maps.Marker({
+                name: that.name,
+                position: {
+                    lat: that.lat,
+                    lng: that.lng
+                },
+                map: map
+            });
+        }
+
     });
 }
 
 function init() {
     var map = new google.maps.Map(document.getElementById('map'), initialStatus);
-    var marker = new google.maps.Marker({
-        name: 'test 1',
-        position: {
-            lat: 39.94,
-            lng: 116.4
-        },
-        map: map,
-    });
+
 }
-fsInit();
+
+
+//google.maps.event.addDomListener(window, 'load', fsInit);
 ko.applyBindings(modelView);
-console.log(markers);
-
-
-// google.maps.event.addDomListener(window, 'load', init);
+console.log(apiResult);
